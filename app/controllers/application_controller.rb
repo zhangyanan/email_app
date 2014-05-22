4316b9ae1301dcbe01f93a21cf4ba3c2266f4cfb
zhangyanan <zhangyanan@huama.com.cn>
2014-05-22 15:15:36 +0800

    设置session user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,9 @@ class ApplicationController < ActionController::Base
   def current_user
     User.find session[:user_id] rescue nil
   end
+
+  def set_session_user user
+    session[:user_id] = user.id
+    session[:p] = []
+  end
 end
