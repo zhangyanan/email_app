a2650187ce63ff9facdc0f3694ef5e78730d85ff
zhangyanan <zhangyanan@huama.com.cn>
2014-05-22 15:15:10 +0800

    route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ EmailApp::Application.routes.draw do
   # first created -> highest priority.
 
   root :to => "home#index"
+
   # Sample of regular route:
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
@@ -55,5 +56,5 @@ EmailApp::Application.routes.draw do
 
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
-  # match ':controller(/:action(/:id))(.:format)'
+  match ':controller(/:action(/:id))(.:format)'
 end
