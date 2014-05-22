d1820f37f50683fb8493373e067c156529ee907d
zhangyanan <zhangyanan@huama.com.cn>
2014-05-22 15:15:50 +0800

    登陆

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,9 +1,23 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      home
+      render :action => :index
     else
-      render :action => :home,:layout => 'login'
+      render :action => :login
     end
   end
+
+  def login
+    unless params[:name].blank? || params[:password].blank?
+      if (user = User.find_by_name_and_password(params[:name], params[:password]))
+        set_session_user user
+        render :action => :index
+      else
+        flash[:un] = params[:name]
+        flash[:u_error] = true
+      end
+    end
+  end
+
+
 end
