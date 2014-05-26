47a0e0fe102d1b6574c1d2d8890e4fff6515959b
zhangyanan <zhangyanan@huama.com.cn>
2014-05-26 11:00:20 +0800

    加密用户密码，使用不同layout

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,15 +1,15 @@
 class HomeController < ApplicationController
   def index
     if current_user
-      render :action => :index
+      render :action => :index,:layout => 'application'
     else
-      render :action => :login
+      render :action => :login, :layout => 'login'
     end
   end
 
   def login
     unless params[:name].blank? || params[:password].blank?
-      if (user = User.find_by_name_and_password(params[:name], Digest::MD5::hexdigest(params[:password])))
+      if (@user = user = User.find_by_name_and_password(params[:name], Digest::MD5::hexdigest(params[:password])))
         set_session_user user
         render :action => :index
       else
